c084dcca2a8379aa6cdc31014db93cd166070cce
Rutvik Chandla <97675949+RutvikChandla@users.noreply.github.com>
2022-02-18 16:06:46 +0530

    Update selenium-3-perl-snippet.pl

--- selenium-3-perl-snippet.pl
+++ selenium-3-perl-snippet.pl
@@ -1,39 +1,38 @@
-<?php
-require_once('vendor/autoload.php');
-use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver\WebDriverExpectedCondition;
-$caps = array(
-    "os" => "OS X",
-    "os_version" => "Sierra",
-    "browser" => "Safari",
-    "browser_version" => "10.0",
-    "browserstack.local" => "false",
-    "browserstack.selenium_version" => "3.5.2",
-    "name" => "Perl iPhone", // test name
-    "build" => "Selenium-3-code-snippet-sanity" // CI/CD job or build name
-);
-$web_driver = RemoteWebDriver::create("https://USER_NAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",$caps);
-try{
-    $web_driver->get("https://bstackdemo.com/");
-    $web_driver->wait(10000)->until(WebDriverExpectedCondition::titleIs("StackDemo"));
-    # getting text of the product
-    $product_on_page = $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='1']/p")))->getText();
-    # clicking the 'Add to cart' button
-    $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='1']/div[4]")))->click();
-    # checking whether the cart pane is present on webpage
-    $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::className("float-cart__content")));
-    # getting text of the product
-    $product_in_cart = $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='__next']/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")))->getText();
-    # Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
-    if ($product_on_page == $product_in_cart){
-        $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Product has been successfully added to the cart!"}}' );
-    }  else {
-        $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Failed to add product to the cart or some elements might have failed to load."}}');
-    }
+use Selenium::Remote::Driver;
+#Input capabilities
+my $extraCaps = {
+  "os_version" => "10.0",
+"device" => "Samsung Galaxy S20",
+"real_mobile" => "true",
+"browserstack.local" => "false",
+  'name' => 'perl Android', # test name
+  'build' => 'Selenium-3-code-snippet-sanity' # CI/CD job or build name
+};
+my $login = "";
+my $key = "";
+my $host = "$login:$key\@hub-cloud.browserstack.com";
+my $driver = new Selenium::Remote::Driver('remote_server_addr' => $host,
+  'port' => '80', 'extra_capabilities' => $extraCaps);
+# Searching for 'BrowserStack' on google.com
+$driver->get('https://bstackdemo.com/');
+my $title =   $driver->get_title();
+
+  # waiting until the Add to Cart button is displayed on webpage and then clicking it
+my $product =   $driver->find_element('//*[@id="1"]/p');
+my $product_text = $product->get_text();
+
+ # waiting until the Add to Cart button is displayed on webpage and then clicking it
+my $cart_btn =   $driver->find_element('//*[@id="1"]/div[4]');
+$cart_btn->click();
+
+my $product_in_cart =   $driver->find_element('//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]');
+my $product_in_cart_text = $product_in_cart->get_text();
+
+# Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
+if ($product_in_cart_text eq $product_text) {
+	$driver->execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Yaay! Title matched!"}}');
+} else {
+	$driver->execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Oops! Title did not match!"}}');
 }
-catch(Exception $e){
-    echo 'Message: ' .$e->getMessage();
-}
-$web_driver->quit();
-?> 
+
+$driver->quit; 
